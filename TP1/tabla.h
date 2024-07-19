2ab67c97faeae82d240fcb216f255272d6f5b1ad
Santiago De Franceschi <sdefranceschi@frba.utn.edu.ar>
2024-07-19 13:06:58 -0300

    Add files via upload

---
+++ TP1/tabla.h
@@ -0,0 +1,10 @@
+#ifndef TABLA_H
+#define TABLA_H
+
+#include <stdio.h>
+#include <assert.h>
+#include "conversion.h"
+
+void armar_tabla(float inicio, float fin, float incremento);
+
+#endif //TABLA_H
